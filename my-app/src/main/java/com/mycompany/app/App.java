5ba7ff772265a5f68cf648caa0aebf29912e9944
sowmianreddy <46764896+sowmianreddy@users.noreply.github.com>
2019-02-12 15:16:24 -0800

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-
+        System.out.println( "To test CI branch commit" );
         System.out.println("A NullPointerException is a drag...");
         Object myObject = null;
         /**
