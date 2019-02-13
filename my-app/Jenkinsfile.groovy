42c074c1cde0ea14237936b4f6ed2445c3adfd80
sowmianreddy <46764896+sowmianreddy@users.noreply.github.com>
2019-02-12 22:42:19 -0800

    Update Jenkinsfile.groovy

--- my-app/Jenkinsfile.groovy
+++ my-app/Jenkinsfile.groovy
@@ -25,7 +25,7 @@ pipeline {
                     sh '''
                  java -cp target/my-app-1.0-SNAPSHOT.jar com.mycompany.app.App
                  '''
-         
+                         }
                     }
                   }
             }
