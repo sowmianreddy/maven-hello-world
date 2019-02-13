70b07d2cd478626cf25729f7040d6b30a505e332
sowmianreddy <46764896+sowmianreddy@users.noreply.github.com>
2019-02-12 22:37:43 -0800

    Update Jenkinsfile.groovy

--- my-app/Jenkinsfile.groovy
+++ my-app/Jenkinsfile.groovy
@@ -20,7 +20,9 @@ pipeline {
             }
             post {
                 success {
-                    junit 'target/surefire-reports/**/*.xml' 
+                 sh '''
+                 java -cp target/my-app-1.0-SNAPSHOT.jar com.mycompany.app.App
+                 '''
                 }
             }
         }
