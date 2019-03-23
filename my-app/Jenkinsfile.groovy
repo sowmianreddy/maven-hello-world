c590230cbc681adf8b0e062ad442e7c5e15a99f9
sowmianreddy <sowmianreddy@gmail.com>
2019-03-23 08:31:47 +0100

    changed

--- my-app/Jenkinsfile.groovy
+++ my-app/Jenkinsfile.groovy
@@ -1,41 +1,43 @@
 pipeline {
         agent any 
-    environment {
-        DISABLE_AUTH = 'true'
-        DB_ENGINE    = 'sqlite'
-    }
+  	  environment {
+        	DISABLE_AUTH = 'true'
+        	DB_ENGINE    = 'sqlite'
+   	 }
         stages {
-        stage ('Initialize') {
-            steps {
-                  sh '''
-                    printenv
-                    export MAVEN_HOME=/opt/apache-maven-3.5.4/bin
-                    export PATH=${MAVEN_HOME}:${PATH}
-                    echo "PATH = ${PATH}"
-                    echo "MAVEN_HOME = ${MAVEN_HOME}"
+		stage ('Initialize') {
+           		 steps {
+                  		sh '''
+                    		printenv
+                    		export MAVEN_HOME=/opt/apache-maven-3.5.4/bin
+                    		export PATH=${MAVEN_HOME}:${PATH}
+                    		echo "PATH = ${PATH}"
+                    		echo "MAVEN_HOME = ${MAVEN_HOME}"
                     
-                '''
-            }
-        }
+                		'''
+            			}
+        	}
 
-        stage ('Build') {
-            steps {
-                dir ("${env.WORKSPACE}/my-app") {
-                    // sh '/opt/apache-maven-3.5.4/bin/mvn -Dmaven.test.failure.ignore=true install' 
-                    sh 'mvn -Dmaven.test.failure.ignore=true install' 
-                }
-            }
-         }
-         stage ('Run') {
-            steps {       
-                  dir ("${env.WORKSPACE}/my-app") {
-                     sh ' java -cp target/my-app-1.0-SNAPSHOT.jar com.mycompany.app.App '
-                  }
-            }      
-         }
-	stage ('build merge job') {
-            build 'demo-2'
-	}
+        	stage ('Build') {
+            		steps {
+                		dir ("${env.WORKSPACE}/my-app") {
+                    		// sh '/opt/apache-maven-3.5.4/bin/mvn -Dmaven.test.failure.ignore=true install' 
+                    		sh 'mvn -Dmaven.test.failure.ignore=true install' 
+                		}
+            		}
+         	}
+         	stage ('Run') {
+            		steps {       
+                  		dir ("${env.WORKSPACE}/my-app") {
+                    		 sh ' java -cp target/my-app-1.0-SNAPSHOT.jar com.mycompany.app.App '
+                 		 }
+            		}      
+        	 }
+		stage ('build merge job') {
+			steps {
+           		 build 'demo-2'
+			}
+		}
         /*
         stage ('Git merge'){
           when {
