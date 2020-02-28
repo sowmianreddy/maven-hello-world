f5e975dc8b5583dec6fa1ce73eca89980ca01806
sowmianreddy <sowmianreddy@gmail.com>
2020-02-28 22:34:31 +0000

    changed in ci branch and merge

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -12,7 +12,7 @@ public class App
         System.out.println( "To test CI branch commit" );
         System.out.println("A NullPointerException is a drag...");
         System.out.println("Another print to test jenkins");
-        System.out.println("checking another ci commit and master merge and master file");
+        System.out.println("ci commit and merge should work");
         Object myObject = null;
         /**
          * Checker prevents this from compiling...
