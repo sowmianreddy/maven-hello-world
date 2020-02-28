1ea90e696b62ca6d3ebbd410967c0ea95e68827a
sowmianreddy <sowmianreddy@gmail.com>
2020-02-28 14:44:50 +0000

    changed in ci branch

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -12,7 +12,7 @@ public class App
         System.out.println( "To test CI branch commit" );
         System.out.println("A NullPointerException is a drag...");
         System.out.println("Another print to test jenkins");
-        System.out.println("Added a new print in pipiline ci");
+        System.out.println("last line is to commit in ci branch");
         Object myObject = null;
         /**
          * Checker prevents this from compiling...
